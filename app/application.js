cf9ba86e217c412e04bc2afb3cf5e508ad2cc243
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2020-03-29 22:11:10 +0200

    environemnts and vlans

--- app/application.js
+++ app/application.js
@@ -5,7 +5,7 @@ var app = Sammy('#main', function () {
 
   this.get('#/', function () {
     this.render('./app/views/home.html', {}).swap();
-
+    activateMenu('#/');
 
     // load some data
 
@@ -16,27 +16,96 @@ var app = Sammy('#main', function () {
     //       .swap();
   });
 
-  this.get("#/vlans", function () {
-    debugger;
-    this.render('./app/views/vlans/list.html', { "items": app.data.vlans }).swap();
+  this.get("#/env", function () {
+    this.render('./app/views/env/list.html', { "items": app.data.env }).swap();
+    activateMenu('#/env');
   });
 
-  this.get("#/vlans/:name", function (name) {
+  this.get("#/env/:name", function (context) {
     var name = this.params['name'];
-    debugger;
-    this.render('./app/views/vlans/details.html', { "name": name }).swap();
+    var item = getItem(app.data.env, 'name', name);
+    this.render('./app/views/env/details.html', item).swap();
+    activateMenu('#/env');
   });
+
+  this.get("#/vlans", function () {
+    this.render('./app/views/vlans/list.html', { "items": app.data.vlans }).swap();
+    activateMenu('#/vlans');
+  });
+
+  this.get("#/vlans/:name", function (context) {
+    var name = this.params['name'];
+    var item = getItem(app.data.vlans, 'name', name);
+    this.render('./app/views/vlans/details.html', item).swap();
+    activateMenu('#/vlans');
+  });
+
+  this.get("#/f5-vs", function () {
+    this.render('./app/views/common/soon.html', { }).swap();
+    activateMenu('#/f5-vs');
+  });
+  
+  this.get("#/firewall-rules", function () {
+    this.render('./app/views/common/soon.html', { }).swap();
+    activateMenu('#/firewall-rules');
+  });
+  
+
+  this.get("#/machine-groups", function () {
+    this.render('./app/views/common/soon.html', { }).swap();
+    activateMenu('#/machine-groups');
+  });
+
+  this.get("#/machines", function () {
+    this.render('./app/views/machines/list.html', { "items": app.data.machines }).swap();
+    activateMenu('#/machines');
+  });
+
+  this.get("#/machines/:name", function (context) {
+    var name = this.params['name'];
+    var item = getItem(app.data.machines, 'name', name);
+    this.render('./app/views/machines/details.html', item).swap();
+    activateMenu('#/machines');
+  });
+
   //this.swap = function(content, callback) {	    	    var context = this;	    context.$element().fadeOut('slow', function() {	        context.$element().html(content);	        context.$element().fadeIn('slow', function() { 	             if (callback) {	                callback.apply();	             }	         }); 	    });	 };
 
+  this.get("#/applications", function () {
+    this.render('./app/views/common/soon.html', { }).swap();
+    activateMenu('#/applications');
+  });
+
+
+  this.get("#/soon", function () {
+    this.render('./app/views/common/soon.html', { }).swap();
+    activateMenu('#/soon');
+  });
 });
 
+
+// start the application
+
 $.get("./app/data/cmdb.json", function (data) {
-  debugger;
-  app.data = data;
+  app.data = postProcessData(data);
   app.run('#/');
 });
 
-  // start the application
 
+function getItem(array, keyName, value) {
+  for(i=0; i<array.length; i++){
+    var item = array[i];
+    if(item[keyName] === value) {
+      return item;
+    }
+  }
+  return null;
+}
 
-  //$('a[src="#/vlans]"')
+function activateMenu(url) {
+  $('#mainNavigation').find('a').removeClass('mm-active');
+  $('#mainNavigation').find('a[href="'+ url + '"]').addClass('mm-active');
+}
+
+function postProcessData(data){
+  return data;
+}
